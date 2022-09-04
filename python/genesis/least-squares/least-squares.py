065e8f5aad47901b139ccef52c90983895bda530
Daniel M. Topa <dantopa@gmail.com>
2022-09-04 18:24:51 -0500

    Create least-squares.py

---
+++ python/genesis/least-squares/least-squares.py
@@ -0,0 +1,7 @@
+#! /home/dantopa/spacktivity/ubuntu-22.04-dantopa-docker-spack/opt/spack/linux-ubuntu22.04-haswell/gcc-12.2.0/python-3.10.6-ybcookynohghdr6i72gwalqle6hq27z5/bin/python
+# Solving Bevington example 6.1 with Julia
+A = [ [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7], [1, 8], [1, 9] ]
+
+T = [15.6, 17.5, 36.6, 43.8, 58.2, 61.6, 64.2, 70.4, 98.8]
+
+xls = A \ T
