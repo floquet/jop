596800bc5684345790b5604457ddf2115da670dc
Daniel M. Topa <dantopa@gmail.com>
2022-09-05 14:33:02 -0500

    Update least-squares.py

--- python/genesis/least-squares/least-squares.py
+++ python/genesis/least-squares/least-squares.py
@@ -2,18 +2,18 @@
 #! /home/dantopa/spacktivity/ubuntu-22.04-dantopa-docker-spack/opt/spack/linux-ubuntu22.04-haswell/gcc-12.2.0/python-3.10.6-ybcookynohghdr6i72gwalqle6hq27z5/bin/python
 #! /home/dantopa/spacktivity/ubuntu-22.04-dantopa-docker-spack/opt/spack/linux-ubuntu22.04-haswell/gcc-12.2.0/python-3.10.6-ybcookynohghdr6i72gwalqle6hq27z5/bin
 # Solving Bevington example 6.1 with Julia
-print( "importing numpy" )
+print( "importing numpy..." )
 import numpy as np
-print( "input design matrix" )
 A = np.array([ [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7], [1, 8], [1, 9] ])
-print( "input data vector" )
+print( "design matrix A\n", np.matrix( A ) )
 T = np.array([15.6, 17.5, 36.6, 43.8, 58.2, 61.6, 64.2, 70.4, 98.8])
-print( "construct Gram matrix" )
+print ( "data vector T\n", np.matrix( T ))
 W = np.dot( A.transpose(), A )
+print( "Gram matrix A*A\n", np.matrix( W ) )
 b = np.dot( A.transpose(), T )
 print( "least squares solution" )
 xls = np.linalg.solve( W, b )
-print (np.matrix(A))
+print ( "least squares solution xls\n", xls )
 
 # dantopa@Quaxolotl.attlocal.net:least-squares $ date
 # Mon Sep  5 12:55:15 CDT 2022
