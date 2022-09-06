00af94201dafb5ef6333937be40c04bfad460a70
Daniel M. Topa <dantopa@gmail.com>
2022-09-05 19:47:56 -0500

    Update least-squares.py

--- python/genesis/least-squares/least-squares.py
+++ python/genesis/least-squares/least-squares.py
@@ -1,6 +1,12 @@
 #! /opt/local/bin/python
 #! /home/dantopa/spacktivity/ubuntu-22.04-dantopa-docker-spack/opt/spack/linux-ubuntu22.04-haswell/gcc-12.2.0/python-3.10.6-ybcookynohghdr6i72gwalqle6hq27z5/bin/python
 # Solving Bevington example 6.1 with Julia
+from datetime import date
+today = date.today( )
+print( today.strftime("%b-%d-%Y") )
+import os
+print(os.path.dirname(os.path.realpath(__file__)))
+
 print( "importing numpy..." )
 import numpy as np
 
@@ -35,7 +41,31 @@ diag  = np.diag( Winv )
 sigma = np.sqrt( t2 / ( m - n ) * diag )
 print ( "sigma =", sigma )
 
+# numerical errors
+print( "\nnumerical errors\n" )
+xlsExact = [ 1733 / 360, 1129 / 120 ]
+numericErrorXLS = xls - xlsExact
+print( "numeric errors, fit parameters" )
+print( numericErrorXLS )
+
+sigmaExact = np.sqrt( np.array( [ 108297055, 3419907 ] ) / 35 ) / 360
+numericErrorSigma = sigma - sigmaExact
+print( "\nnumeric errors, sigma parameters" )
+print( numericErrorSigma )
+
+
+# machine epsilon
+machineEpsilon = np.finfo(float).eps
+print( "\nmachine epsilon =", np.finfo(float).eps )
+numericErrorXLSeps   = numericErrorXLS   / machineEpsilon
+numericErrorXLSsigma = numericErrorSigma / machineEpsilon
+print( "\nnumeric epsilons, fit parameters:   ", numericErrorXLSeps )
+print( "\nnumeric epsilons, sigma parameters: ", numericErrorXLSsigma )
+
 # dantopa@Quaxolotl.attlocal.net:least-squares $ ./least-squares.py
+# Sep-05-2022
+# /Volumes/T7-Touch/repos/github/jop/python/genesis/least-squares
+
 # importing numpy...
 # design matrix A
 #  [[1 1]
@@ -52,3 +82,21 @@ print ( "sigma =", sigma )
 #  [[  9  45]
 #  [ 45 285]]
 # least squares solution xls = [4.81388889 9.40833333]
+# pseudoinverse        xpinv = [4.81388889 9.40833333]
+# least total squared error = 316.6580555555554
+# A matrix dimensions = [9, 2]
+# sigma = [4.88620631 0.86830165]
+#
+# numerical errors
+#
+# numeric errors, fit parameters
+# [ 9.76996262e-15 -1.77635684e-15]
+#
+# numeric errors, sigma parameters
+# [-8.88178420e-16 -2.22044605e-16]
+#
+# machine epsilon = 2.220446049250313e-16
+#
+# numeric epsilons, fit parameters:    [44. -8.]
+#
+# numeric epsilons, sigma parameters:  [-4. -1.]
