75e3b2657534d573b61d80404c7b2c14a7fd17bf
Daniel M. Topa <dantopa@gmail.com>
2022-09-05 20:32:44 -0500

    Update least-squares.py

--- python/genesis/least-squares/least-squares.py
+++ python/genesis/least-squares/least-squares.py
@@ -1,6 +1,6 @@
 #! /opt/local/bin/python
 #! /home/dantopa/spacktivity/ubuntu-22.04-dantopa-docker-spack/opt/spack/linux-ubuntu22.04-haswell/gcc-12.2.0/python-3.10.6-ybcookynohghdr6i72gwalqle6hq27z5/bin/python
-# Solving Bevington example 6.1 with Julia
+# Solving Bevington example 6.1 with Python
 from datetime import date
 today = date.today( )
 print( today.strftime("%b-%d-%Y") )
